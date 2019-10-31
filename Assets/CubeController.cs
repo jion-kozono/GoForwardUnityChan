7569c0e220eb8b54e90008237a701452e5c44b89
jion-kozono <zeon66190329@gmail.com>
2019-10-31 18:05:18 +0900

    課題：キューブの接触時に効果音をつけましょう 2019_10_31

---
+++ Assets/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        // 地面に接触する時とキューブ同士が積み重なるときはボリュームを0.1にする（追加）
+        if (other.gameObject.tag == "cubTag" || other.gameObject.tag == "groundTag")
+        {
+            GetComponent<AudioSource>().volume = 0.1f;
+        }
+    }
+}
